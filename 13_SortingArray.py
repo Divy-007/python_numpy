225e8dfc9f70489d9b92daae80db563b2c07909d
Divy kushwaha <work.divy07@gmail.com>
2025-06-03 18:20:53 +0530

    implementing sorting in 1-d amd 2-D arrays in row wisw ,column wise (alphabetically also)

---
+++ 13_SortingArray.py
@@ -0,0 +1,40 @@
+#Sorting - Sorting is techinique to reordering the array inn specifuc  order
+
+import numpy as np 
+devu=np.array([3,4,2,1])
+print(np.sort(devu))
+
+# sorting alphabeticaly  - we can sort the array alphabectically 
+
+import numpy as np
+devu=np.array(["car", "air" , "zebta"])
+print(np.sort(devu))
+
+#Sorting boolean - we can sort boolean statement 
+import numpy as np
+devu=np.array([True,False]) 
+print(np.sort(devu))
+
+#Sorting 2-D array
+
+import numpy as np
+devu=np.array([[2,4,1,3,6,4],
+               [3,58,2,48,28,0]])
+print(np.sort(devu))
+
+
+      #sorting in flat manner all row in one 
+import numpy as np
+devu=np.array([[2,4,1,3,6,4],
+               [3,58,2,48,28,0]])
+print(np.sort(devu ))
+    #sorting in column manner
+import numpy as np
+devu=np.array([[2,4,1,3,6,4],
+               [3,58,2,48,28,0]])
+print(np.sort(devu,axis=0 ))
+
+      
+
+
+
