3349936362b005922bc2e4075a428019ee9b8964
Divy kushwaha <work.divy07@gmai.com>
2025-07-27 15:49:12 +0530

    lets visulize through seaborn

---
+++ 18_Seaborn.py
@@ -0,0 +1,9 @@
+"""
+"seaborn is python library based on matplotlib used for stastical graph"
+"""
+import seaborn as sns
+import matplotlib.pyplot as plt
+x=[1,2,3,4,5]
+sns.displot(x, kde=True)
+plt.show()
+
